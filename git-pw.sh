60c31087ab70641628d37e315f8c678602e8b974
Shaun Ostoic <ostoic@uwindsor.ca>
2023-01-11 19:21:41 -0500

    git-pw add merge, update

--- git-pw.sh
+++ git-pw.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# TODO: git-like operations pull, merge, update, etc
 
 PW="Passwords.kdbx"
 EPW=$PW.gpg
@@ -8,30 +9,62 @@ WORK=$(mktemp -d)
 pushd $WORK
 
 # Backup current pw
-cp ~/$PW .current-pw.kdbx.bk
-git clone git@github.com:Ostoic/personal.git
-cp personal/$EPW git-pw.kdbx.gpg
+backup-current-pw() {
+  cp ~/$1 ~/."$(date +%s)"-current-pw.kdbx.bk
+}
 
-# Decrypt secrets from git repo
-gpg -d -o git-pw.kdbx git-pw.kdbx.gpg
+git-clone-pw() {
+  git clone git@github.com:Ostoic/personal.git
+  # cp personal/$1 git-pw.kdbx.gpg
 
-cp ~/$PW .
+  # Decrypt secrets from git repo
+  gpg -d -o git-pw.kdbx "personal/$1.gpg"
+}
 
-# Merge the current pw and git pws
-keepassxc-cli merge -s $PW git-pw.kdbx
-if [ "$(sha256sum $PW)" = "$(sha256sum ~/$PW)" ]; then
-  echo "No changes"
-	exit
+merge-into-current() {
+  # Merge the databases: (git, pw) -> pw
+  keepassxc-cli merge -s $1 git-pw.kdbx
+  if [ "$(sha256sum $1)" = "$(sha256sum ~/$1)" ]; then
+    echo "No changes"
+    exit
+  fi
+}
+
+push-git() {
+  pushd personal
+  git add .
+  git commit -m 'kdbx update'
+  git push
+  popd
+}
+
+git-encrypt-pw() {
+  rm "personal/$1.gpg"
+  gpg -c -o "personal/$1.gpg" $1
+}
+
+if [ $# == 0 ]; then
+  OP="merge"
+else
+  OP=$1
 fi
 
-gpg -c -o $EPW $PW
-cp $EPW personal/$EPW
+backup-current-pw $PW
+git-clone-pw $PW
+cp ~/$PW .
 
-pushd personal
-git add .
-git commit -m 'kdbx update'
-git push
+# Encrypt pw into personal/$PW.gpg
+git-encrypt-pw $PW
 
-popd 
-cp $PW ~/
+if [ $OP == "merge" ]; then
+  merge-into-current $PW
+  push-git
+
+  # Move merged database into the user's home directory
+  cp $PW ~/
+
+elif [ $OP == "push" ]; then
+  push-git
+fi
+
 rm -rf $WORK
