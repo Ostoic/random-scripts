eedca12f528a0286bebd637dddceaff3b970216d
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2023-09-05 03:00:37 -0400

    Add runner spawner capabilities

--- run-github-worker.sh
+++ run-github-worker.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-GITHUB_REPO="https://github.com/Ostoic/whispe-rs-types"
+REPO_PATH="https://github.com/ostoic/$GITHUB_REPO"
+#$GITHUB_BEARER
+
+create-worker-token() {
+	REPO=$1
+	RESULT=$(curl -sL \
+		-X POST \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer $GITHUB_BEARER" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		https://api.github.com/repos/ostoic/$REPO/actions/runners/registration-token 2>&1)
+
+	echo -n $RESULT | xargs | cut -d' ' -f3 | tr -dc '[a-zA-Z0-9]'
+}
 
 run-worker() {
 	USER=$1
 	WORKER_TOKEN=$2
 
-	ssh $USER@ci-server "cd actions-runner && config.cmd remove --local && config.cmd --url $GITHUB_REPO --token $WORKER_TOKEN --work _work$RANDOM --name devops$RANDOM --unattended --ephemeral && run.cmd"
+	ssh $USER@ci-server "cd actions-runner && config.cmd remove --local && config.cmd --url $REPO_PATH --token $WORKER_TOKEN --work _work$RANDOM --name devops$RANDOM --unattended --ephemeral && run.cmd"
 }
 
-run-worker $1 $2
+set -e
+TOKEN=$(create-worker-token $GITHUB_REPO)
+run-worker $1 $TOKEN
