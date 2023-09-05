c24b74839ba147f56762c0d7f2f175fbd88dc510
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2023-09-05 03:16:01 -0400

    Make runner work directory not random
    - This effectively causes a memory leak.

--- run-github-worker.sh
+++ run-github-worker.sh
@@ -10,7 +10,7 @@ create-worker-token() {
 		-H "Accept: application/vnd.github+json" \
 		-H "Authorization: Bearer $GITHUB_BEARER" \
 		-H "X-GitHub-Api-Version: 2022-11-28" \
-		https://api.github.com/repos/ostoic/$REPO/actions/runners/registration-token 2>&1)
+		https://api.github.com/repos/Ostoic/$REPO/actions/runners/registration-token 2>&1)
 
 	echo -n $RESULT | xargs | cut -d' ' -f3 | tr -dc '[a-zA-Z0-9]'
 }
@@ -19,7 +19,7 @@ run-worker() {
 	USER=$1
 	WORKER_TOKEN=$2
 
-	ssh $USER@ci-server "cd actions-runner && config.cmd remove --local && config.cmd --url $REPO_PATH --token $WORKER_TOKEN --work _work$RANDOM --name devops$RANDOM --unattended --ephemeral && run.cmd"
+	ssh $USER@ci-server "cd actions-runner && config.cmd remove --local && config.cmd --url $REPO_PATH --token $WORKER_TOKEN --work _workt --name devops$RANDOM --unattended --ephemeral && run.cmd"
 }
 
 set -e
